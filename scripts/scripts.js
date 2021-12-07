d732e9ffd2283e66eed979190d55fc7d17b8f706
bhail300 <bhaile300@gmail.com>
2021-12-06 22:18:06 -0800

    Further updated Html, CSS and added JS
    Almost completed Html and CSS. Need to clean up layout and structure. Also need more images at some point.

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+/*INSTRUCTION POP-UP*/
+const rule_box  = document.getElementById("rule-box");
+const close_rule = document.getElementById("close-rule");
+const rule_btn  = document.getElementById("rule-btn");
+
+rule_box.style.display = "none";
+
+$(rule_btn).click(function(){
+   setTimeout( function(){
+     $(rule_box).fadeIn(900);
+    });  
+});
+
+$(close_rule).click(function(){
+    setTimeout( function(){
+      $(rule_box).fadeOut(900);
+     });  
+ });
